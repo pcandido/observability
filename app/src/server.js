3c41699b29df82978bdac0c43db2f420327173fe
Paulo Cândido <paulo.candido@g.globo>
2022-11-15 15:10:50 -0300

    feat: create an image to app

--- app/src/server.js
+++ app/src/server.js
@@ -1,7 +1,11 @@
+const http = require('http')
 const express = require('express')
 
-const port = parseInt(process.env.PORT ?? 8080)
 const app = express()
+
+app.get('/health-check', (req, res) => {
+  res.status(200).end()
+})
 
 app.get('/', (req, res) => {
   const clientIp = req.headers['x-forwarded-for'] || req.socket.remoteAddress
@@ -9,6 +13,7 @@ app.get('/', (req, res) => {
   res.send('Hello world!!!')
 })
 
-app.listen(port, () => {
+const port = parseInt(process.env.PORT ?? 8080)
+http.createServer(app).listen(port, '0.0.0.0', () => {
   console.log(`listening on http://localhost:${port}`)
 })
