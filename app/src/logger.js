648c5851ab585c55e2aea07a868182d2e2ec3120
Paulo Cândido <paulo.candido@g.globo>
2022-11-18 00:06:04 -0300

    feat: create an endpoint to app (calculate fibonacci)

--- app/src/logger.js
+++ app/src/logger.js
@@ -6,7 +6,7 @@ const logDir = process.env.LOG_DIR ?? path.join(__dirname, '..', 'logs')
 const logger = winston.createLogger({
   level: process.env.LOG_LEVEL ?? 'info',
   format: winston.format.json(),
-  defaultMeta: { service: 'app' },
+  defaultMeta: { application: 'app' },
   transports: [
     new winston.transports.File({ filename: path.join(logDir, 'error.log'), level: 'error' }),
     new winston.transports.File({ filename: path.join(logDir, 'app.log') }),
--- app/src/server.js
+++ app/src/server.js
@@ -1,25 +1,51 @@
 const http = require('http')
 const express = require('express')
+const cors = require('cors')
 const logger = require('./logger')
 
 const app = express()
+app.use(cors())
 
 app.get('/health-check', (req, res) => {
   res.status(200).send('App is up!')
 })
 
-app.get('/', (req, res) => {
-  const clientIp = req.headers['x-forwarded-for'] || req.socket.remoteAddress
+app.get('/fibonacci/:value', (req, res) => {
+  const client = req.headers['x-forwarded-for'] || req.socket.remoteAddress
+  const value = req.params['value']
 
   const shouldSimulateError = Math.random() < 0.05
   if (shouldSimulateError) {
     res.status(500).end()
-    logger.error(`request from ${clientIp}: fail`)
+    logger.error(`an error ocurred`, { client, value, headers: req.headers })
   } else {
-    res.send('Hello world!!!')
-    logger.info(`request from ${clientIp}: success`)
+    const start = new Date().getTime()
+    const seq = fibonacciSec(value)
+    const end = new Date().getTime()
+    const spendTime = end - start
+
+    res.json(seq)
+    logger.info(`processed fibonacci of ${value} in ${spendTime}ms`, { client, value, spendTime, headers: req.headers })
   }
 })
+
+// purposefully inefficient
+function fibonacciSec(num) {
+  const data = []
+  for (let i = 0; i < num; i++) {
+    data.push(fibonacci(i))
+  }
+  return data
+}
+
+function fibonacci(num) {
+  if (num < 2) {
+    return num
+  }
+  else {
+    return fibonacci(num - 1) + fibonacci(num - 2)
+  }
+}
 
 const port = parseInt(process.env.PORT ?? 8080)
 http.createServer(app).listen(port, '0.0.0.0', () => {
